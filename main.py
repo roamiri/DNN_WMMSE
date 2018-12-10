c3f65e421e98ae8c228ffcfd98873159bc3954b0
Roohollah Amiri <amiriruhollah@gmail.com>
2018-12-09 18:04:06 -0700

    started working on test data, added test data

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ data_file = open("./data/Ytrain.pickle","rb")
 Ytrain = pickle.load(data_file)
 data_file.close()
 
+data_file = open("./data/Xeval.pickle","rb")
+Xeval = pickle.load(data_file)
+data_file.close()
+
+data_file = open("./data/Yeval.pickle","rb")
+Yeval = pickle.load(data_file)
+data_file.close()
+
 data_file = open("./data/Xtest.pickle","rb")
 Xtest = pickle.load(data_file)
 data_file.close()
@@ -32,6 +40,13 @@ training_data = []
 for i in range(0, Xtrain.shape[1]/5):
     training_data.append((scaler.fit_transform(Xtrain[:, i].reshape(100, 1)), Ytrain[:, i].reshape(10, 1)))
 
+for i in range(0, Yeval.shape[1]):
+    Yeval[:, i][Yeval[:, i] < 0.5] = 0.0
+    Yeval[:, i][Yeval[:, i] >= 0.5] = 1.0
+
+eval_data = []
+for i in range(0, Xeval.shape[1]/5):
+    eval_data.append((scaler.fit_transform(Xeval[:, i].reshape(100, 1)), Yeval[:, i].reshape(10, 1)))
 
 for i in range(0, Ytest.shape[1]):
     Ytest[:, i][Ytest[:, i] < 0.5] = 0.0
@@ -44,23 +59,24 @@ for i in range(0, Xtest.shape[1]/5):
 data_size = len(training_data)
 mini_batch_size = 10
 training_epochs = 90
-eta = 5.0
+eta = 1.0
 lmbda = 0.0
 
-DNN = network.Network([K**2, 200, 100, 50, K], cost=network.QuadraticCost)
+DNN = network.Network([K**2, 200, 100, K], cost=network.CrossEntropyCost)
 
 # evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = []
-
+T = False
 (evaluation_cost, evaluation_accuracy, training_cost, training_accuracy) = DNN.SGD(training_data, training_epochs, mini_batch_size, eta, lmbda, test_data,
-        monitor_evaluation_cost=False,
-        monitor_evaluation_accuracy=False,
-        monitor_training_cost=False,
-        monitor_training_accuracy=False
+        monitor_evaluation_cost=T,
+        monitor_evaluation_accuracy=T,
+        monitor_training_cost=T,
+        monitor_training_accuracy=T
         )
 
-save = True
+save = False
 if save:
-    with open('./data/three_{0}_{1}_{2}_{3}_{4}.pkl'.format('200', eta, lmbda, mini_batch_size, data_size), 'wb') as f:
+    # with open('./data/three_{0}_{1}_{2}_{3}_{4}.pkl'.format('200,200,200', eta, lmbda, mini_batch_size, data_size), 'wb') as f:
+    with open('./data/Quadratic.pkl','wb') as f:
         pickle.dump(training_accuracy, f)
         pickle.dump(training_cost, f)
         pickle.dump(evaluation_accuracy, f)
@@ -84,10 +100,10 @@ if plotOutput:
     plt.xlabel('Epochs')
     plt.grid(True, color='k', linestyle=':', linewidth=0.3)
     axes = plt.gca()
-    ymax = max(training_accuracy)
-    xpos = training_accuracy.index(ymax)
-    axes.annotate('max={}%'.format(ymax), xy=(xpos, ymax), xytext=(xpos-10, ymax - 10),arrowprops=dict(facecolor='black', shrink=0.05))
+    # ymax = max(training_accuracy)
+    # xpos = training_accuracy.index(ymax)
+    # axes.annotate('max={}%'.format(ymax), xy=(xpos, ymax), xytext=(xpos-10, ymax - 10),arrowprops=dict(facecolor='black', shrink=0.05))
     # axes.set_xlim([xmin, xmax])
     axes.set_ylim([0.0, 100.0])
-    axes.legend(['Training data', 'Evaluation data'], loc='upper left')
+    axes.legend(['Training data', 'Test data'], loc='upper left')
     plt.show()
